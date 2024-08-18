de0027d28d1cc71559711ffe0d34ffdba27aafc3
Ethan Glasser-Camp <ethan@betacantrips.com>
2024-08-18 13:56:31 -0400

    Silly program that uses an effect

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,27 @@
+import Cleff
+import Effects.Interact
 import Effects.Teletype
 
+chat :: (Interact :> es) => Eff es ()
+chat = do
+  display "What's your name?"
+  name <- inputText
+  display $ "Nice to meet you, " ++ name ++ "!"
+  display "Do you like airplanes?"
+  plane <- yesOrNo
+  if not plane
+    then display "Me either! Personally, I don't mind takeoffs -- it's the landings that scare me!"
+    else do
+      display "Do you fly first class?"
+      rich <- yesOrNo
+      if rich
+        then display "Wow! Those tech salaries are really something!"
+        else display "Well, maybe one day after the IPO!"
+  display $ "It was nice talking to you, " ++ name ++ ". Hope you enjoy your trip!"
+
 main :: IO ()
-main = pure ()
+main =
+  runIOE
+    . runTeletypeIO
+    . runInteractTeletype
+    $ chat
