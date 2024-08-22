8c403c23ab6359ecb7d6dae3eefec971cfc38a5e
Ethan Glasser-Camp <ethan@betacantrips.com>
2024-08-22 02:42:06 -0400

    Add tests for the new code path

--- test/SampleProgramSpec.hs
+++ test/SampleProgramSpec.hs
@@ -49,3 +49,46 @@ spec = describe "chat" $ do
               runInteractTalker ethan chat
     Set.toList users
       `shouldBe` [ethanRecord]
+  it "talks correctly to old users" $ do
+    let (_, messages) =
+          runPure . runState [] . outputToListState $
+            runUserStorePureDiscardResult (Set.singleton ethanRecord) $
+              runInteractTalker ethan chat
+    reverse messages
+      `shouldBe` [ "What's your name?"
+                 , "Good to see you again, Ethan!"
+                 , "Do you still like airplanes?"
+                 , "Thanks for confirming!"
+                 , "Are you flying first class yet?"
+                 , "Thanks for confirming!"
+                 , "It was nice talking to you, Ethan. Hope you enjoy your trip!"
+                 ]
+  describe "when users change their feelings" $ do
+    let newTalker =
+          ethan
+            { respondYesNo = \s -> case s of
+                _ | "airplanes" `isInfixOf` s -> True
+                _ | "first class" `isInfixOf` s -> True
+                _ -> False
+            }
+    it "talks correctly" $ do
+      let (_, messages) =
+            runPure . runState [] . outputToListState $
+              runUserStorePureDiscardResult (Set.singleton ethanRecord) $
+                runInteractTalker newTalker chat
+      reverse messages
+        `shouldBe` [ "What's your name?"
+                   , "Good to see you again, Ethan!"
+                   , "Do you still like airplanes?"
+                   , "Thanks for confirming!"
+                   , "Are you flying first class yet?"
+                   , "Thanks for updating us!"
+                   , "It was nice talking to you, Ethan. Hope you enjoy your trip!"
+                   ]
+    it "updates user store" $ do
+      let (_, users) =
+            runPure . (ignoreOutput :: Eff (Output String : es) a -> Eff es a) $
+              runUserStorePure mempty $
+                runInteractTalker newTalker chat
+      Set.toList users
+        `shouldBe` [ethanRecord {rich = True}]
