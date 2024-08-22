adeb55c5c2023522bfb3bb327dec965d16c884da
Ethan Glasser-Camp <ethan@betacantrips.com>
2024-08-22 02:07:34 -0400

    Look up user and store new ones

--- src/SampleProgram.hs
+++ src/SampleProgram.hs
@@ -7,13 +7,27 @@ import Effects.UserStore
 chat :: ([Interact, UserStore] :>> es) => Eff es ()
 chat = do
   name <- promptText "What's your name?"
+  userM <- lookupUserByName name
+  case userM of
+    Nothing -> do
+      inputUser <- readUserFromInteract name
+      storeUser inputUser
+      display $ "It was nice talking to you, " ++ name ++ ". Hope you enjoy your trip!"
+    Just user ->
+      pure ()
+
+readUserFromInteract :: (Interact :> es) => String -> Eff es UserRecord
+readUserFromInteract name = do
   display $ "Nice to meet you, " ++ name ++ "!"
   plane <- promptYesOrNo "Do you like airplanes?"
   if not plane
-    then display "Me either! Personally, I don't mind takeoffs -- it's the landings that scare me!"
+    then do
+      display "Me either! Personally, I don't mind takeoffs -- it's the landings that scare me!"
+      -- FIXME: find another way to identify the affluence of the user
+      pure UserRecord {name = name, flies = plane, rich = False}
     else do
       rich <- promptYesOrNo "Do you fly first class?"
       if rich
         then display "Wow! Those tech salaries are really something!"
         else display "Well, maybe one day after the IPO!"
-  display $ "It was nice talking to you, " ++ name ++ ". Hope you enjoy your trip!"
+      pure UserRecord {name = name, flies = plane, rich = rich}
